83c82e0fe453a939db8cddc464fcf30016e82839
Kevin Shao <shaoqihen@gmail.com>
2021-03-23 09:56:10 -0400

    updated period

--- .vscode/Programs/Debug and Tests/test.jl
+++ .vscode/Programs/Debug and Tests/test.jl
@@ -164,7 +164,7 @@ end
         
     end
 
-    return minimum(periodicity_error[2:end]), r, v #don't return early phases when close to start
+    return argmin(periodicity_error[2:end]), minimum(periodicity_error[2:end]), r, v #don't return early phases when close to start
 end
 
 
@@ -398,22 +398,24 @@ end
 
 #phase2_v(r,v,m)
 
+#search 2
 function phase0_am(r,v,m)#refine angular velocities
 
-    am_results = zeros(Float128, (2001, 2)) #initialize results
-    zarray = LinRange(0.09, 0.11, 2001)
-
+    am_results = zeros(Float128, (2001, 3)) #initialize results
+    zrange = LinRange(-0.00001, 0.00001, 2001)
     for i in 1:2001
-        am_results[i,1] = copy(zarray[i])
+        am_results[i,1] = copy(zrange[i])
+        am_results[i,1] += 9.85900000000000109601216990995453671e-02
     end
+    zarray = am_results[:,1]
     #search iteration
-    for i in 667:667
+    for i in 1:1
     
         core2_intv = copy(v) #initialize core velocities
         core3_intv = copy(v)
         core4_intv = copy(v)
         
-        
+        println(zarray[i])
         core2_intv[2,3] += zarray[i]#grid search parameters
         core2_intv[3,3] -= zarray[i]
         core3_intv[2,3] += zarray[i+667]
@@ -426,21 +428,24 @@ function phase0_am(r,v,m)#refine angular velocities
         coarse3 = remotecall(run,3, r, core3_intv, m,  1e-3,30,1000, r, core3_intv)
         coarse4 = remotecall(run,4, r, core4_intv, m,  1e-3,30,1000, r, core4_intv)
         
-        coarse2_p, coarse2_r, coarse2_v = fetch(coarse2) #fetch coarse
-        coarse3_p, coarse3_r, coarse3_v = fetch(coarse3)
-        coarse4_p, coarse4_r, coarse4_v = fetch(coarse4)
+        coarse2_p, coarse2_e, coarse2_r, coarse2_v = fetch(coarse2) #fetch coarse
+        coarse3_p, coarse3_e, coarse3_r, coarse3_v = fetch(coarse3)
+        coarse4_p, coarse4_e, coarse4_r, coarse4_v = fetch(coarse4)
         
         fine2 = remotecall(run,2, coarse2_r, coarse2_v, m, 1e-3,80,1, r, core2_intv)#fine simulation
         fine3 = remotecall(run,3, coarse3_r, coarse3_v, m,  1e-3,80,1, r, core3_intv)
         fine4 = remotecall(run,4, coarse4_r, coarse4_v, m,  1e-3,80,1, r, core4_intv)
         
-        fine2_p, fine2_r, fine2_v = fetch(fine2) #fetch fine
-        fine3_p, fine3_r, fine3_v = fetch(fine3)
-        fine4_p, fine4_r, fine4_v = fetch(fine4)
+        fine2_p, fine2_e, fine2_r, fine2_v = fetch(fine2) #fetch fine
+        fine3_p, fine3_e, fine3_r, fine3_v = fetch(fine3)
+        fine4_p, fine4_e, fine4_r, fine4_v = fetch(fine4)
 
-        am_results[i, 2] = fine2_p #save periodicity error into results
-        am_results[i+667, 2] = fine3_p
-        am_results[i+1334, 2] = fine4_p
+        am_results[i, 2] = fine2_e #save periodicity error into results
+        am_results[i+667, 2] = fine3_e
+        am_results[i+1334, 2] = fine4_e
+        am_results[i, 3] = fine2_p #save periodicity error into results
+        am_results[i+667, 3] = fine3_p
+        am_results[i+1334, 3] = fine4_p
         println("progress = ",i,"/667")
     end
 
@@ -452,8 +457,8 @@ function phase0_am(r,v,m)#refine angular velocities
     println("z =",am_results[row,1])
     println("minimum error =",minimum(am_results[:,2]))
     df = convert(DataFrame,am_results)
-    name = string("C:\\Users\\shaoq\\Documents\\GitHub\\rebound\\.vscode\\Programs\\Grid Search\\Grid Search Data\\Grid Search 4.0\\Phase0AM_3_22.csv")
-    rename!(df,[:"Vz",:"periodicity error"])
+    name = string("C:\\Users\\shaoq\\Documents\\GitHub\\rebound\\.vscode\\Programs\\Grid Search\\Grid Search Data\\Grid Search 4.0\\Phase0AM_3_23(2).csv")
+    rename!(df,[:"Vz",:"periodicity error",:"period"])
     CSV.write(name,df)
 
     println("DONE")
